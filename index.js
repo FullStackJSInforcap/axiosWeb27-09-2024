df3ac22577e2f87d7f6cd00c4363ce615b533a3f
oe.marcos <marcos.ojeda.escalante@gmail.com>
2024-09-30 10:34:30 -0300

    get pos put delete

--- index.js
+++ index.js
@@ -18,3 +18,17 @@ axios.post('http://localhost:3000/clientes', {
     .then((datos) => {
         console.log(datos.data);
     });
+
+axios.put('http://localhost:3000/clientes', {
+    id: 6,
+    nombre: "probando nombre put",
+    apellido: "probando apellido put"
+})
+    .then((datos) => {
+        console.log(datos.data);
+    });
+
+axios.delete('http://localhost:3000/clientes/7')
+    .then((datos) => {
+        console.log(datos.data);
+    });
